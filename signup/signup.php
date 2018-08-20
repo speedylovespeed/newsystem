6c3ad06632bc67908e68541d21ce88e7320619ad
user-PC\user <markjohnfuerzas@gmail.com>
2018-08-20 20:02:14 +0800

    Initial commit

---
+++ signup/signup.php
@@ -0,0 +1,13 @@
+<?php
+	include_once '../header/header.php';
+?>
+
+	<section class="main-container">
+		<div class="main-wrapper">
+			<h2>Sign Up</h2>
+		</div>
+	</section>
+<?php
+	include_once '../footer/footer.php';
+?>
+
